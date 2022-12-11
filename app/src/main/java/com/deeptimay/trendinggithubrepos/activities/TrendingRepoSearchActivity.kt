54ea91f0129f7656881d1a703ec1ca973e22aae8
Deeptimay Routray <deeptimay.r@medibuddy.in>
2022-12-11 23:44:08 +0530

    search fixes

--- app/src/main/java/com/deeptimay/trendinggithubrepos/activities/TrendingRepoSearchActivity.kt
+++ app/src/main/java/com/deeptimay/trendinggithubrepos/activities/TrendingRepoSearchActivity.kt
@@ -1,31 +1,24 @@
 package com.deeptimay.trendinggithubrepos.activities
 
-import android.graphics.Color
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
-import android.widget.AdapterView
-import android.widget.ArrayAdapter
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.SearchView
 import androidx.appcompat.widget.Toolbar
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
-import androidx.paging.ItemSnapshotList
 import androidx.paging.LoadState
 import com.deeptimay.trendinggithubrepos.R
 import com.deeptimay.trendinggithubrepos.R.layout.activity_trending_repository
 import com.deeptimay.trendinggithubrepos.adapter.ReposAdapter
 import com.deeptimay.trendinggithubrepos.adapter.ReposLoadStateAdapter
-import com.deeptimay.trendinggithubrepos.data.model.Languages
-import com.deeptimay.trendinggithubrepos.data.model.Repo
 import com.deeptimay.trendinggithubrepos.databinding.ActivityTrendingRepositoryBinding
 import com.deeptimay.trendinggithubrepos.util.hide
 import com.deeptimay.trendinggithubrepos.util.show
 import com.deeptimay.trendinggithubrepos.viewModels.ReposViewModel
 import dagger.hilt.android.AndroidEntryPoint
-import java.util.*
+
 
 @AndroidEntryPoint
 class TrendingRepoSearchActivity : AppCompatActivity() {
@@ -33,7 +26,6 @@ class TrendingRepoSearchActivity : AppCompatActivity() {
     private lateinit var viewModel: ReposViewModel
     private lateinit var binding: ActivityTrendingRepositoryBinding
     lateinit var reposAdapter: ReposAdapter
-    private var repoList: ItemSnapshotList<Repo>? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -107,35 +99,28 @@ class TrendingRepoSearchActivity : AppCompatActivity() {
         val searchItem = menu?.findItem(R.id.action_search)
         val searchView = searchItem?.actionView as SearchView
 
-        val searchAutoComplete = searchView.findViewById(androidx.appcompat.R.id.search_src_text) as SearchView.SearchAutoComplete
-
-        // Get SearchView autocomplete object
-        searchAutoComplete.setTextColor(Color.BLACK)
-        searchAutoComplete.setDropDownBackgroundResource(R.color.colorSecondary)
-
-        val newsAdapter: ArrayAdapter<String> = ArrayAdapter(
-            this,
-            R.layout.dropdown_item,
-            Languages.data
-        )
-        searchAutoComplete.setAdapter(newsAdapter)
-
-        // Listen to search view item on click event
-        searchAutoComplete.onItemClickListener =
-            AdapterView.OnItemClickListener { adapterView, _, itemIndex, _ ->
-                val queryString = adapterView.getItemAtPosition(itemIndex) as String
-                Toast.makeText(this, "search: ", Toast.LENGTH_SHORT).show()
-                searchAutoComplete.setText(String.format(getString(R.string.search_query), queryString))
-                binding.rvRepository.scrollToPosition(0)
-                val languageQuery = String.format(getString(R.string.query), queryString)
-                viewModel.searchRepos(languageQuery)
-
-//                repoList = reposAdapter.snapshot()
-//
-//                reposAdapter.submitData(this.lifecycle, repoList.)
-                searchView.clearFocus()
-                (this as AppCompatActivity).supportActionBar?.title = queryString.replaceFirstChar { if (it.isLowerCase()) it.titlecase(Locale.ROOT) else it.toString() }
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String): Boolean {
+                callSearch(query)
+                return true
             }
+
+            override fun onQueryTextChange(newText: String): Boolean {
+                callSearch(newText)
+                return true
+            }
+
+            fun callSearch(query: String?) {
+                if (query.isNullOrEmpty())
+                    viewModel.searchRepos("Q")
+                query?.let {
+                    if (it.length > 3)
+                        viewModel.searchRepos(it)
+                    else if (it.isEmpty())
+                        viewModel.searchRepos("Q")
+                }
+            }
+        })
         return true
     }
 
